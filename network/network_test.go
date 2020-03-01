2f2de391bed263e90ecd52842d4aaac10d09660f
Roberto Mier Escandon <rmescandon@gmail.com>
2020-03-01 18:45:31 +0100

    Add tests for connected status check

--- network/network_test.go
+++ network/network_test.go
@@ -37,7 +37,6 @@ func (s *NetworkSuite) TearDownTest(c *check.C) {
 }
 
 func (s *NetworkSuite) TestGetAvailableSsids(c *check.C) {
-
 	devs := []string{
 		"WifiDevice1",
 		"WifiDevice2",
@@ -103,4 +102,90 @@ func (s *NetworkSuite) TestGetAvailableSsids(c *check.C) {
 	for _, flag := range reachableSsids {
 		c.Assert(flag, check.Equals, false)
 	}
+}
+
+func (s *NetworkSuite) TestIsConnected(c *check.C) {
+	devs := []string{
+		"WifiDevice1",
+		"WifiDevice2",
+		"OtherDevice3",
+	}
+
+	nWifiDevices := 2
+	nNotWifiDevices := 1
+
+	// Get All devices once
+	s.mockBusObject.EXPECT().Call("org.freedesktop.NetworkManager.GetAllDevices", gomock.Any()).Return(
+		&dbus.Call{
+			Body: []interface{}{devs},
+		})
+
+	// Two devices are wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(uint32(2)),
+		nil,
+	).Times(nWifiDevices)
+
+	// One device is not wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(uint32(18)),
+		nil,
+	).Times(nNotWifiDevices)
+
+	// Get the Connected state
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.State").Return(
+		dbus.MakeVariant(uint32(100)),
+		nil,
+	)
+
+	// Execute the test
+	nm, err := network.NewManager()
+	c.Assert(err, check.IsNil)
+
+	b, err := nm.Connected()
+	c.Assert(err, check.IsNil)
+	c.Assert(b, check.Equals, true)
+}
+
+func (s *NetworkSuite) TestIsDisconnected(c *check.C) {
+	devs := []string{
+		"WifiDevice1",
+		"WifiDevice2",
+		"OtherDevice3",
+	}
+
+	nWifiDevices := 2
+	nNotWifiDevices := 1
+
+	// Get All devices once
+	s.mockBusObject.EXPECT().Call("org.freedesktop.NetworkManager.GetAllDevices", gomock.Any()).Return(
+		&dbus.Call{
+			Body: []interface{}{devs},
+		})
+
+	// Two devices are wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(uint32(2)),
+		nil,
+	).Times(nWifiDevices)
+
+	// One device is not wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(uint32(18)),
+		nil,
+	).Times(nNotWifiDevices)
+
+	// Get the Connected state
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.State").Return(
+		dbus.MakeVariant(uint32(200)),
+		nil,
+	).Times(nWifiDevices)
+
+	// Execute the test
+	nm, err := network.NewManager()
+	c.Assert(err, check.IsNil)
+
+	b, err := nm.Connected()
+	c.Assert(err, check.IsNil)
+	c.Assert(b, check.Equals, false)
 }
