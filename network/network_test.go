7fab222dc95b47bc2efddea53e591fe0ade08839
Roberto Mier Escandon <rmescandon@gmail.com>
2020-04-10 12:56:21 +0200

    Add connect unit test

--- network/network_test.go
+++ network/network_test.go
@@ -15,7 +15,7 @@ const (
 	// any value other than 2
 	notWifiDeviceType uint32 = 18
 	// any value other than 100
-	notWifiDeviceConnected uint32 = 200
+	wifiDeviceNotConnected uint32 = 200
 )
 
 var testCtx *testing.T
@@ -184,7 +184,7 @@ func (s *NetworkSuite) TestIsDisconnected(c *check.C) {
 
 	// Get the Connected state
 	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.State").Return(
-		dbus.MakeVariant(uint32(notWifiDeviceConnected)),
+		dbus.MakeVariant(uint32(wifiDeviceNotConnected)),
 		nil,
 	).Times(nWifiDevices)
 
@@ -195,4 +195,114 @@ func (s *NetworkSuite) TestIsDisconnected(c *check.C) {
 	b, err := nm.Connected()
 	c.Assert(err, check.IsNil)
 	c.Assert(b, check.Equals, false)
+}
+
+func (s *NetworkSuite) TestConnect(c *check.C) {
+	// TODO asuming no previous connection and manager enabled. Try to cover the rest of cases
+	reachableSsids := map[string]bool{
+		"MyHomeWifi":     false,
+		"MyNeigbourWifi": false,
+	}
+
+	devs := []string{
+		"WifiDevice1",
+		"WifiDevice2",
+		"OtherDevice3",
+	}
+
+	aps := []string{
+		"AccessPoint1",
+		"AccessPoint2",
+	}
+
+	nWifiDevices := 2
+	nNotWifiDevices := 1
+
+	// Is network enabled
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.NetworkingEnabled").Return(
+		dbus.MakeVariant(true),
+		nil,
+	)
+
+	// Get All devices once
+	s.mockBusObject.EXPECT().Call("org.freedesktop.NetworkManager.GetAllDevices", gomock.Any()).Return(
+		&dbus.Call{
+			Body: []interface{}{devs},
+		})
+
+	// Two devices are wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(uint32(network.WifiDeviceType)),
+		nil,
+	).Times(nWifiDevices)
+
+	// One device is not wifi
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.DeviceType").Return(
+		dbus.MakeVariant(notWifiDeviceType),
+		nil,
+	).Times(nNotWifiDevices)
+
+	// Get the Connected state
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.State").Return(
+		dbus.MakeVariant(uint32(wifiDeviceNotConnected)),
+		nil,
+	)
+
+	// NO PREVIOUS Available connections
+	s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.Device.AvailableConnections").Return(
+		dbus.MakeVariant([]dbus.ObjectPath{}),
+		nil,
+	)
+
+	// Get the access points
+	s.mockBusObject.EXPECT().Call("org.freedesktop.NetworkManager.Device.Wireless.GetAccessPoints", gomock.Any()).Return(
+		&dbus.Call{
+			Body: []interface{}{aps},
+		})
+
+	for ssid, _ := range reachableSsids {
+		s.mockBusObject.EXPECT().GetProperty("org.freedesktop.NetworkManager.AccessPoint.Ssid").Return(
+			dbus.MakeVariant(ssid),
+			nil,
+		)
+	}
+
+	// Listen to connected event
+	devObjPath := dbus.ObjectPath("/org/freedesktop/NetworkManager/Devices/6")
+	s.mockBusObject.EXPECT().Path().Return(devObjPath).Times(2)
+
+	apObjPath := dbus.ObjectPath("/org/freedesktop/NetworkManager/AccessPoint/18")
+	s.mockBusObject.EXPECT().Path().Return(apObjPath)
+
+	s.mockBusObject.EXPECT().AddMatchSignal(
+		"org.freedesktop.NetworkManager.Device", "StateChanged", dbus.WithMatchObjectPath(devObjPath)).Return(
+		&dbus.Call{},
+	)
+
+	// Connect
+	ssidToConnectTo := "MyNeigbourWifi"
+	pskToConnectTo := "myneighbourwifipassword"
+	sec := "802-11-wireless-security"
+	keyMgt := "wpa-psk"
+	st := map[string]map[string]dbus.Variant{
+		"801-11-wireless": map[string]dbus.Variant{
+			"security": dbus.MakeVariant(sec),
+		},
+		"802-11-wireless-security": map[string]dbus.Variant{
+			"key-mgmt": dbus.MakeVariant(keyMgt),
+			"psk":      dbus.MakeVariant(pskToConnectTo),
+		},
+	}
+
+	s.mockBusObject.EXPECT().Call(
+		"org.freedesktop.NetworkManager.AddAndActivateConnection", gomock.Any(), st, devObjPath, apObjPath).Return(
+		&dbus.Call{},
+	)
+
+	// Execute the test
+	nm, err := network.NewManager()
+	c.Assert(err, check.IsNil)
+
+	_, err = nm.Connect(ssidToConnectTo, pskToConnectTo, sec, keyMgt)
+	c.Assert(err, check.IsNil)
 }
